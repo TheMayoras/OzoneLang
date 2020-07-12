86405bbb17ae2a5d2b1b34d3eee844f4daa6d958
Valio Valtokari <ubuntugeek1904@gmail.com>
2020-07-12 12:22:38 +0300

    added tests

---
+++ src/test/java/ozonelang/ozone/test/ExceptionTest.java
@@ -0,0 +1,19 @@
+package ozonelang.ozone.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static ozonelang.ozone.core.AST.exception.OzoneException.raiseEx;
+import ozonelang.ozone.core.AST.exception.OzoneException;
+import ozonelang.ozone.core.AST.Location;
+import ozonelang.ozone.core.AST.exception.StackTrace;
+
+public class ExceptionTest {
+    @Test
+    public void testException() {
+        StackTrace s = new StackTrace(new Location("<test>", "oh boi", 1L));
+        for (int i = 0; i < 10; i++)
+            s.addLocation(new Location("<test>", "oh no", (long) i));
+        OzoneException e = new OzoneException("TestException", s, true);
+        raiseEx(e);
+    }
+}
