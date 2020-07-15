bc4bb6d092f4c3f9a807f33d247e92766495f61b
Valio Valtokari <ubuntugeek1904@gmail.com>
2020-07-15 10:03:33 +0300

    Fixed mistakes

--- src/main/java/ozonelang/ozone/core/AST/node/CodeNode.java
+++ src/main/java/ozonelang/ozone/core/AST/node/CodeNode.java
@@ -3,13 +3,9 @@ package ozonelang.ozone.core.AST.node;
 import ozonelang.ozone.core.AST.Context;
 import ozonelang.ozone.core.AST.Span;
 
-import static ozonelang.ozone.core.runtime.exception.OzoneException.raiseEx;
-
 public abstract class CodeNode implements Span {
     private final Context context;
     public CodeNode(Context context) {
-        if (start.getStartLine() != end.getStartLine())
-            raiseEx(new RuntimeException("cannot have two different files in a codeNode"), true, start, end);
         this.context = context;
     }
     @Override
@@ -19,6 +15,6 @@ public abstract class CodeNode implements Span {
 
     @Override
     public String toString() {
-        return String.format("<%s@%s, %d-%d>", getClass().getCanonicalName(), start.getFile(), start.getStartLine(), end.getStartLine());
+        return String.format("<%s@%s, %d-%d>", getClass().getCanonicalName(), context.getFile(), context.getStartLine(), context.getEndLine());
     }
 }
