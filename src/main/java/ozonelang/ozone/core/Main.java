86f67cea0162511bf68e3f4ca640bc640caf6341
Valio Valtokari <ubuntugeek1904@gmail.com>
2020-07-21 17:31:21 +0300

    Switched to JFlex! writing a new parser soon

--- src/main/java/ozonelang/ozone/core/Main.java
+++ src/main/java/ozonelang/ozone/core/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
         String line = "";
-        while (!line.equals("exit")) {
+        /*while (!line.equals("exit")) {
             line = s.nextLine();
             new Parser().parse(line, "<stdin>");
-        }
+        }*/
     }
 }
